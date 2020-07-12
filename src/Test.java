132e4c5ccbea85d3ef26f6a4a9e4a1f2f39dfb0a
TianZY <1438472761@qq.com>
2020-07-12 08:52:46 +0800

    修改.gitignore

--- src/Test.java
+++ src/Test.java
@@ -9,9 +9,10 @@ import javax . swing . Timer ;
 import java.util.List;
 public class Test {
     public static void main(String[] args) {
-        String str="cat dog dog catt",pattern = "abba";
-        int num=81;
+        int[][] left={{1,0,1},{1,1,0},{1,1,0}};
+        int[] right={4};
+        int num=9;
         Solution a=new Solution();
-        System.out.println(a.isPowerOfThree(num));
+        System.out.println(a.numSubmat(left).toString());
     }
 }
